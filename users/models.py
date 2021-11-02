a7f0a0c1b641836813d59b9b1efff6e86e3e7064
Andrew Smith <a.smithb21@gmail.com>
2021-11-02 15:25:22 -0500

    added custom user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,11 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.crypto import get_random_string
 
 
 class CustomUser(AbstractUser):
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    friend_code = models.CharField(default=get_random_string, unique=True)
+    friends = models.ManyToManyField('self', blank=True)
+    blocked_users = models.ManyToManyField('self', blank=True) 
