ec1a9bafa18d5f9e3486839a5bec7ec013273d45
Andrew Smith <a.smithb21@gmail.com>
2021-11-02 14:56:15 -0500

    first commit

---
+++ instant_messanger_app/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for instant_messanger_app project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instant_messanger_app.settings')
+
+application = get_asgi_application()
