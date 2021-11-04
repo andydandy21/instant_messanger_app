651a74916e0bad8d5091842100fe40caadd2b22e
Andrew Smith <a.smithb21@gmail.com>
2021-11-04 17:15:29 -0500

    added CustomUser tests

--- users/models.py
+++ users/models.py
@@ -1,4 +1,3 @@
-from typing_extensions import Required
 import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -6,9 +5,10 @@ from django.utils.crypto import get_random_string
 
 
 class CustomUser(AbstractUser):
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    friend_code = models.CharField(default=get_random_string(length=10), unique=True)
-    friends = models.ManyToManyField('self', blank=True, required=False)
-    pending_friend_request = models.ManyToManyField('self', blank=True, required=False)
-    blocked_users = models.ManyToManyField('self', blank=True, required=False) 
-    profile_picture = models.ImageField(required=False)
+    friend_code = models.CharField(default=get_random_string, max_length=12, unique=True)
+    friends = models.ManyToManyField('self', blank=True)
+    pending_friend_requests = models.ManyToManyField('self', blank=True)
+    blocked_users = models.ManyToManyField('self', blank=True) 
+    profile_picture = models.ImageField(default='default.png')
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,61 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+
+    def setUp(self):
+
+        User = get_user_model()
+        self.friend = User.objects.create_user(
+            username='frienduser',
+            email='frienduser@email.com',
+            password='testpass123',
+        )
+        self.pending_friend = User.objects.create_user(
+            username='pendingfriend',
+            email='pendingfriend@email.com',
+            password='testpass123',
+        )
+        self.blocked_user = User.objects.create_user(
+            username='blockeduser',
+            email='blockeduser@email.com',
+            password='testpass123',
+        )
+        self.custom_user = User.objects.create_user(
+            username='testuser',
+            email='testuser@email.com',
+            password='testpass123',
+        )
+        self.custom_user.friends.add(self.friend)
+        self.custom_user.pending_friend_requests.add(self.pending_friend)
+        self.custom_user.blocked_users.add(self.blocked_user)
+        self.admin_user = User.objects.create_superuser(
+            username='admin',
+            email='admin@email.com',
+            password='testpass123',
+        )
+
+    def test_create_user(self):
+
+        self.assertEqual(self.custom_user.username, 'testuser')
+        self.assertEqual(self.custom_user.email, 'testuser@email.com')
+        self.assertEqual(self.custom_user.profile_picture, 'default.png')
+        self.assertEqual(len(self.custom_user.friend_code), 12)
+        self.assertEqual(self.custom_user.friends.first().username, 'frienduser')
+        self.assertEqual(self.custom_user.pending_friend_requests.first().username, 'pendingfriend')
+        self.assertEqual(self.custom_user.blocked_users.first().username, 'blockeduser')
+        self.assertTrue(self.custom_user.is_active)
+        self.assertFalse(self.custom_user.is_staff)
+        self.assertFalse(self.custom_user.is_superuser)
+
+
+    def test_create_superuser(self):
+
+        self.assertEqual(self.admin_user.username, 'admin')
+        self.assertEqual(self.admin_user.email,'admin@email.com')
+        self.assertTrue(self.admin_user.is_active)
+        self.assertTrue(self.admin_user.is_staff)
+        self.assertTrue(self.admin_user.is_superuser)
+        
+
