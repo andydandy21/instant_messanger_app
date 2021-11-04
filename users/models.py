f7cd127e7081170cb2fc8b8f0bdbf91a3fd6212d
Andrew Smith <a.smithb21@gmail.com>
2021-11-04 13:55:57 -0500

    added profile picture and pending friend request fields to custom user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from typing_extensions import Required
 import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -6,6 +7,8 @@ from django.utils.crypto import get_random_string
 
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    friend_code = models.CharField(default=get_random_string, unique=True)
-    friends = models.ManyToManyField('self', blank=True)
-    blocked_users = models.ManyToManyField('self', blank=True) 
+    friend_code = models.CharField(default=get_random_string(length=10), unique=True)
+    friends = models.ManyToManyField('self', blank=True, required=False)
+    pending_friend_request = models.ManyToManyField('self', blank=True, required=False)
+    blocked_users = models.ManyToManyField('self', blank=True, required=False) 
+    profile_picture = models.ImageField(required=False)
