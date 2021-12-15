deeac2c23c722643089729eff5f206bf9b157514
Andrew Smith <a.smithb21@gmail.com>
2021-12-15 10:55:17 -0600

    added random_string function to specify length on get_random_string

--- users/models.py
+++ users/models.py
@@ -3,13 +3,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.crypto import get_random_string
 
+def random_string():
+    return get_random_string(length=12)
 
 class CustomUser(AbstractUser):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=150, unique=False)
     email = models.EmailField(blank=False, unique=True)
-    friend_code = models.CharField(default=get_random_string, max_length=12, unique=True, editable=False)
+    friend_code = models.CharField(default=random_string, max_length=12, unique=True, editable=False)
     friends = models.ManyToManyField('self', blank=True, related_name='friends')
     pending_friend_requests = models.ManyToManyField('self', blank=True, related_name='friend_requests')
     blocked_users = models.ManyToManyField('self', blank=True, related_name='blocked_users') 
