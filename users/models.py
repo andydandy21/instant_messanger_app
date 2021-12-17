7c07ac2b48f78b7da98e822d9eb552b6d41f5a49
Andrew Smith <a.smithb21@gmail.com>
2021-12-17 09:48:02 -0600

    added profile_name to users.CustomUser for non-unique usernames

--- users/models.py
+++ users/models.py
@@ -9,13 +9,10 @@ def random_string():
 class CustomUser(AbstractUser):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(max_length=150, unique=False)
+    profile_name = models.CharField(max_length=150, unique=False, default='anonymous')
     email = models.EmailField(blank=False, unique=True)
     friend_code = models.CharField(default=random_string, max_length=12, unique=True, editable=False)
     friends = models.ManyToManyField('self', blank=True, related_name='friends')
     pending_friend_requests = models.ManyToManyField('self', blank=True, related_name='friend_requests')
     blocked_users = models.ManyToManyField('self', blank=True, related_name='blocked_users') 
     profile_picture = models.ImageField(default='default.png')
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
